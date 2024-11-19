3487709692650175cd57d2512cccc8eb16650f08
dipakhiwarkar <dipakhiwarkar96@gmail.com>
2024-11-19 15:07:28 +0530

    Added some flux methods

--- reactive-programming/src/main/java/com/dps/reactive_programming/ReactiveProgramm.java
+++ reactive-programming/src/main/java/com/dps/reactive_programming/ReactiveProgramm.java
@@ -4,7 +4,9 @@ import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Locale;
 
 public class ReactiveProgramm {
 
@@ -20,16 +22,56 @@ public class ReactiveProgramm {
     {
         List<String> list=List.of("Dipak","Nar","dadad","tatya");
 
+        Flux<Integer> integerFlux=Flux.range(1,20);
+        Flux<Integer> filter = integerFlux.filter(i -> i % 2 == 0);
+       filter.subscribe(System.out::println);
         return Flux.fromIterable(list);
     }
 
-    public static void main(String[] args) {
+
+    private Flux<String> textmap()
+    {
+        List<String> list=List.of("Dipak","Nar","dadad","tatya");
+        Flux<String> flux=Flux.just("Dipak","Nar","dadad","tatya");
+        return  flux.map(s->s.toUpperCase(Locale.ROOT));
+    }
+
+    private Flux<String> testFlatMap()
+    {
+        List<String> list=List.of("Dipak","Nar","dadad","tatya");
+        Flux<String> flux=Flux.just("Dipak","Nar","dadad","tatya");
+        return  flux.flatMap(s->Mono.just(s.toUpperCase(Locale.ROOT)));
+    }
+
+    private Flux<String> testSkip()
+    {
+
+        Flux<String> flux=Flux.just("Dipak","Nar","dadad","tatya");
+        return  flux.skip(2);
+    }
+
+    private Flux<String> testDelay()
+    {
+
+        Flux<String> flux=Flux.just("Dipak","Nar","dadad","tatya");
+        return  flux.delayElements(Duration.ofSeconds(1));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
 
         ReactiveProgramm reactiveProgramm=new ReactiveProgramm();
          //reactiveProgramm.testMono().subscribe(data->System.out.println(data));
 
-         reactiveProgramm.testFlux().subscribe(data-> System.out.println(data));
+        // reactiveProgramm.testFlux().subscribe(data-> System.out.println(data));
 
+       // reactiveProgramm.textmap().subscribe(System.out::println);
+
+        //reactiveProgramm.testFlatMap().subscribe(System.out::println);
+
+       // reactiveProgramm.testSkip() .subscribe(System.out::println);
+
+        /*reactiveProgramm.testDelay() .subscribe(System.out::println);
+        Thread.sleep(100000);*/
 
 
     }
